00be9ca7727590f297697fe39e4c356ab8b85167
carolineberlemont <carolineberlemont@gmail.com>
2019-02-08 08:29:32 +0100

    Ajout du fichier .htaccess

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-require_once '/path/to/vendor/autoload.php';
+// require_once '/path/to/vendor/autoload.php';
 
 
 class HomeController extends Controller {    
